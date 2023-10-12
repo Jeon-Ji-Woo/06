0ff4a3b174c0c868c9a406085b8f673f83d526f1
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-10-12 15:51:13 +0900

    실습1

--- main.c
+++ main.c
@@ -3,13 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 	
-void func1(void){
-	int x;
-	printf("func1 x is at %p\n", &x);
+void print_star()
+{
+	int i;
+	for(i=0; i < 10; i++)
+		printf("*");
 }
-int main(void){
-	int x;
-	printf("main x is at %p\n", &x);
-	func1();
-	return 0;
+int main(void)
+{
+	print_star();
+	print_star();
+	print_star();
+	
+  return 0;
 }
