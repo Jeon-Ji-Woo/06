b12d0507fe87542001793261576fcebb118fa1ab
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-10-12 16:18:59 +0900

    실습4-1

--- main.c
+++ main.c
@@ -3,33 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 	
-int sumTwo(int a, int b)
+void square(int a)
 {
-	int c = a + b;
-	return c;
+	a = a*a;
 }
 
-int square(int n)
+int main()
 {
-	return n*n;
-}
-int get_max(int x, int y)
-{
-	if (x > y)
-		return x;
-	return y;
-}
-int main(void)
-{
-	int a, b;
-	int n;
+	int a = 2;
+	square(a);
+	printf("a=%i\n",a);
 	
-	a=3;
-	b=10;
-	n=9;
-	printf("sumTwo result : %i\n", sumTwo(a,b) );
-	
-	printf("square reslut : %i\n", square(n) );
-	
-	printf("get_max result : %i\n", get_max(a,b) );
 }
+
+
